56e2e520792885aace4dcf1f24c006501b3a5f3e
satyamprajap <satyamprajapati9532@gmail.com>
2026-03-27 18:40:59 +0530

    Update UserInput.java
    userinput

--- UserInput.java
+++ UserInput.java
@@ -1,7 +1,28 @@
 import java.util.Scanner;
 
-public class UserInput{
-    public static void main(String[] args){
-        
+public class UserInputDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking integer input
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        // Taking string input
+        sc.nextLine(); // clear buffer
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        // Taking float input
+        System.out.print("Enter your marks: ");
+        float marks = sc.nextFloat();
+
+        // Displaying output
+        System.out.println("\n--- User Details ---");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+
+        sc.close();
     }
 }
