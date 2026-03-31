2a50807c946dd51f01848c10b7d6451cdaa326de
satyamprajap <satyamprajapati9532@gmail.com>
2026-04-01 00:49:19 +0530

    Update Pattern1.java

--- Pattern1.java
+++ Pattern1.java
@@ -1,10 +1,41 @@
+
+// Star Square Pattern
 public class Pattern1 {
+    
     public static void main(String[] args) {
         int n = 4;
 
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= n; j++) {
                 System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+//Right Triangle Pattern
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+//Number Triangle
+
+public class Pattern4 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
