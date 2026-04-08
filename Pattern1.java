5beb7ca634b81b753b6c04f46c52dc46880f4ec2
satyamprajap <satyamprajapati9532@gmail.com>
2026-04-08 23:22:36 +0530

    Update  program

--- Pattern1.java
+++ Pattern1.java
@@ -41,3 +41,94 @@ public class Pattern4 {
         }
     }
 }
+    
+//1. Sum of Natural Numbers Series
+
+//👉 Series: 1 + 2 + 3 + ... + n
+
+import java.util.Scanner;
+
+public class Series1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, sum = 0;
+
+        System.out.print("Enter value of n: ");
+        n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum of series = " + sum);
+    }
+}
+
+//2. Sum of Squares Series
+
+//👉 Series: 1² + 2² + 3² + ... + n²
+
+import java.util.Scanner;
+
+public class Series2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, sum = 0;
+
+        System.out.print("Enter n: ");
+        n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            sum += i * i;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
+
+//3. Factorial Series
+
+//👉 Series: 1! + 2! + 3! + ... + n!
+
+import java.util.Scanner;
+
+public class Series3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        long sum = 0, fact = 1;
+
+        System.out.print("Enter n: ");
+        n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+            sum += fact;
+        }
+
+        System.out.println("Sum of factorial series = " + sum);
+    }
+}
+
+//4. Harmonic Series
+
+//👉 Series: 1 + 1/2 + 1/3 + ... + 1/n
+
+import java.util.Scanner;
+
+public class Series4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        double sum = 0;
+
+        System.out.print("Enter n: ");
+        n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            sum += 1.0 / i;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
