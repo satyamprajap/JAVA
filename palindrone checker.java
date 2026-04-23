84583385f99d1ea7d0ca36eaea0d9a13ce54c45b
satyamprajap <satyamprajapati9532@gmail.com>
2026-04-23 23:00:34 +0530

    Update  program

---
+++ palindrone checker.java
@@ -0,0 +1,44 @@
+//palindrone  checker using Deqcue
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class PalindromeDeque {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        // Create a deque
+        Deque<Character> deque = new LinkedList<>();
+
+        // Add characters to deque
+        for (int i = 0; i < input.length(); i++) {
+            deque.addLast(input.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        // Check palindrome
+        while (deque.size() > 1) {
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
+
+            if (front != rear) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Output result
+        if (isPalindrome) {
+            System.out.println("It is a Palindrome.");
+        } else {
+            System.out.println("It is NOT a Palindrome.");
+        }
+
+        sc.close();
+    }
+}
+
